f07fbc3b18664f23d14493dea9028b476f2c6d0f
jleo3 <joseph.leo3@gmail.com>
2015-05-03 17:19:39 -0400

    test is green

--- spec/jasmine_fixture_builder_spec.rb
+++ spec/jasmine_fixture_builder_spec.rb
@@ -4,8 +4,5 @@ describe JasmineFixtureBuilder do
   it 'has a version number' do
     expect(JasmineFixtureBuilder::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
+
