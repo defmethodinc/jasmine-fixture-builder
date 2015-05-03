c667f56bd4bf5f636fc407b185d040ff1eec77f9
jleo3 <joseph.leo3@gmail.com>
2015-05-03 16:59:12 -0400

    Add `rspec-rails` dependency

--- lib/jasmine_fixture_builder.rb
+++ lib/jasmine_fixture_builder.rb
@@ -1,5 +1,13 @@
 require "jasmine_fixture_builder/version"
 
 module JasmineFixtureBuilder
-  # Your code goes here...
+  RSpec::Matchers.define :save_fixture do |markup, fixture|
+    fixture_path = Rails.root.join('spec/javascripts/fixtures')
+    Dir.mkdir(fixture_path) unless File.exists?(fixture_path)
+    fixture_file = File.join(fixture_path, fixture)
+    File.open(fixture_file, 'w') do |file|
+      file.puts(markup)
+    end
+  end
 end
+
